3da620b20f866bc0f01a22712bb44082eb34923f
boazanton@gmail.com <boazanton@gmail.com@4e62193a-709c-11de-83e1-4fc93b4a6815>
2011-06-26 19:21:28 +0000

    version 0.2.0

---
+++ tags/0.2.0/NLog.Targets.FireDotNet/Meta.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2011 Boaz den Besten. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using System;
+using System.Web.Script.Serialization;
+
+namespace NLog.Targets.FireDotNet
+{
+
+    class Meta
+    {
+
+        [ScriptIgnore]
+        public LogLevel Level { get; set; }
+
+        public string Type
+        {
+            get { return Level.ToString(); }
+        }
+
+        public string File { get; set; }
+        
+        public int Line { get; set; }
+
+        public string Label { get; set; }
+
+    }
+}
